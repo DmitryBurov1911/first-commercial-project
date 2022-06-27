4071b3213d3ac14d0bd3ff68d74f6a05d25c6935
Dmitry <99758072+DmitryBurov1911@users.noreply.github.com>
2022-06-27 17:15:14 +0300

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -137,5 +137,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "dnnd1264@gmail.com"
-EMAIL_HOST_PASSWORD = "kolobanga333"
+EMAIL_HOST_PASSWORD = "your_password"
 EMAIL_USE_TLS = True
